59f776b4a8453ccc381e5ca1b605f8246af58b23
Ryan Welch <welchr@umich.edu>
2019-04-30 15:20:27 -0400

    Trying to use only WASM w/o runtime

--- src/mvtdst/wasm.sh
+++ src/mvtdst/wasm.sh
@@ -11,7 +11,7 @@ source /Users/welchr/scratch/emsdk/emsdk_env.sh
 
 # Compile to WASM
 # Having a .html extension on the -o file makes emscripten generate .js, .html, and .wasm
-em++ --bind -std=c++11 -Os -I../f2c/libf2c/llvm/emscripten -s WASM=1 -o mvtdstpack.html mvtdstpack.cpp pmvnorm.cpp ../f2c/libf2c/llvm/emscripten/libf2c.a -lm
+em++ --bind -std=c++11 -Os -I../f2c/libf2c/llvm/emscripten -s WASM=1 -o mvtdstpack.wasm mvtdstpack.cpp pmvnorm.cpp ../f2c/libf2c/llvm/emscripten/libf2c.a -lm
 
 # This command line is useful for compiling with debug support. It also generates source code maps.
 #em++ --bind -std=c++11 -g4 -I../f2c/libf2c/llvm/emscripten -s WASM=1 -s ASSERTIONS=2 -s DEMANGLE_SUPPORT=1 -o mvtdstpack.html mvtdstpack.cpp pmvnorm.cpp ../f2c/libf2c/llvm/emscripten/libf2c.a -lm
